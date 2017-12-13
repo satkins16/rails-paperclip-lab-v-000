4883d3846e28cac9fe2689b195d14d5ee62ceb87
Scott Atkins <satkins16@gmail.com>
2017-12-13 02:56:46 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_attached_file :album_cover
+  has_attached_file :album_cover, styles: { thumb: "100x100>" }
   validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
